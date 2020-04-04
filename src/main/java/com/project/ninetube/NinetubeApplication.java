af551fee0d2a642f0a4449c6a334414c31f9ab30
Hong <gtv7854@naver.com>
2020-04-04 16:43:45 +0900

    SETTING

---
+++ src/main/java/com/project/ninetube/NinetubeApplication.java
@@ -0,0 +1,13 @@
+package com.project.ninetube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinetubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinetubeApplication.class, args);
+    }
+
+}
