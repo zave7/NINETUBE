8a16f041c10843afeb80873f969f092c012b6fb0
Hong <gtv7854@naver.com>
2020-04-11 16:17:14 +0900

    TEST2

--- src/main/java/com/project/ninetube/main/homeController.java
+++ src/main/java/com/project/ninetube/main/homeController.java
@@ -2,6 +2,7 @@ package com.project.ninetube.main;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class homeController {
@@ -9,5 +10,11 @@ public class homeController {
     @RequestMapping("/")
     public String home() {
         return "Hello, Spring boot!";
+    }
+
+    @RequestMapping("/test")
+    public ModelAndView createQuestion(){
+        ModelAndView mv = new ModelAndView("jsp/test");
+        return mv;
     }
 }
