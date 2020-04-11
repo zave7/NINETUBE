92717e2479b6b7f96cbf9608a7b41702604ad7c4
Hong <gtv7854@naver.com>
2020-04-11 17:04:18 +0900

    schema

---
+++ src/test/java/com/project/ninetube/NinetubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ninetube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinetubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
