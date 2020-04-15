ae93a2b389fa933183187f7ec40bf920b464a600
jaeyeon23 <catblues282@gmail.com>
2020-04-16 00:35:54 +0900

    2020.04.16 관리자 페이지 index.jsp

--- src/main/java/com/project/ninetube/main/homeController.java
+++ src/main/java/com/project/ninetube/main/homeController.java
@@ -2,6 +2,7 @@ package com.project.ninetube.main;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class homeController {
